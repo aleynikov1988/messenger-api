508a1d75df2a05ed7a47ebce3ce2271f26d7649f
Maxim Aleynikov <max.aleyinikov@gmail.com>
2022-01-24 08:59:21 +0200

    initial commit

---
+++ src/main/kotlin/org/messenger/api/ApiApplication.kt
@@ -0,0 +1,11 @@
+package org.messenger.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<ApiApplication>(*args)
+}
